54a4a9e4f7cb1dca7f92ee0870f96a10db48fa60
Mohak Acharya <Mohak@Homer.local>
2023-01-25 15:32:53 -0700

    Added calculator.cpp
    Working perfectly

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+signed main()
+{
+	int v1, v2; 
+	cin>>v1>>v2; 
+	cout<<"Addition: "<<v1+v2<<endl;   
+	cout<<"Subtraction: "<<v1-v2<<endl;
+	cout<<"Multiplication: "<<v1*v2<<endl;
+	cout<<"Division: "<<v1/v2<<endl;
+	return 0; 
+
+   
+}
